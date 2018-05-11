f5f1e2b54b09caa5f1f48c9eebbc39166abb5f7f
William Wu <dev.wwu@gmail.com>
2018-05-11 15:10:47 -0700

    Continued working on level generator

--- Shout of Mission/Assets/LevelGenerator.cs
+++ Shout of Mission/Assets/LevelGenerator.cs
@@ -20,7 +20,7 @@ public class LevelGenerator : MonoBehaviour {
     WNE,
     NESW
   };
-  private MapModule[] modules = { MapModule.Empty, MapModule.NS, MapModule.EW, MapModule.NE, 
+  private MapModule[] modules = new MapModule[] { MapModule.Empty, MapModule.NS, MapModule.EW, MapModule.NE, 
     MapModule.ES, MapModule.SW, MapModule.WN, MapModule.NES, MapModule.ESW, MapModule.SWN, 
     MapModule.WNE, MapModule.NESW };
   private MapModule[,] upConnections;
@@ -41,21 +41,58 @@ public class LevelGenerator : MonoBehaviour {
 	}
 
   void InitializeModules () {
-    //
+    MapModule[] southConnectors = new MapModule[] { MapModule.NS, MapModule.ES, MapModule.SW, MapModule.NES, MapModule.ESW, MapModule.SWN, MapModule.NESW };
+
+    upConnections = new MapModule[,] {
+      { MapModule.Empty, MapModule.EW, MapModule.NE, MapModule.WN, MapModule.WNE },
+      { MapModule.NS, MapModule.ES, MapModule.SW, MapModule.NES, MapModule.ESW, MapModule.SWN, MapModule.NESW },
+      { MapModule.Empty, MapModule.EW, MapModule.NE, MapModule.WN, MapModule.WNE },
+      { MapModule. }
+    };
   }
 
   void InitializeMap () {
-    //
+    map = new int[rows, cols];
+    for (int c = 0; c < cols; c++) {
+      map[0, c] = 0;
+      map[rows - 1, c] = 0;
+    }
+    for (int r = 1; r < rows - 1; r++) {
+      map[r, 0] = 0;
+      map[r, cols - 1] = 0;
+    }
+
+    Random.seed = System.DateTime.Now.Millisecond;
+    for (int r = 1; r < rows - 1; r++) {
+      for (int c = 1; c < cols - 1; c++) {
+        List<MapModule> valid = GetValidMapModules (r, c);
+        map[r, c] = valid[Random.Range (0, valid.Length)];
+      }
+    }
   }
 
-  List<MapModule> getValidMapModules (int row, int col) {
+  List<MapModule> GetValidMapModules (int row, int col) {
     List<MapModule> valid = new List<MapModule>();
 
     for (int i = 0; i < modules.Length; i++) {
       MapModule m = modules[i];
+      if (upConnections[i].Contains(ToObject(map[row - 1, col])) &&
+          downConnections[i].Contains(ToObject(map[row + 1, col])) &&
+          leftConnections[i].Contains(ToObject(map[row, col - 1])) &&
+          rightConnections[i].Contains(ToObject(map[row, col + 1]))) {
+        valid.Add(m);
+      }
+    }
+
+    if (valid.Length == 0) {
+      valid.Add(MapModule.Empty);
     }
 
     return valid;
+  }
+
+  MapModule ToModule (int x) {
+    return (MapModule)Enum.ToObject(typeof(MapModule), x);
   }
 
   void DisplayMap () {
