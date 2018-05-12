e88f002a984da01027ce3995837992de525897f8
William Wu <dev.wwu@gmail.com>
2018-05-11 20:10:53 -0700

    Continued working on level generation

--- Shout of Mission/Assets/LevelGenerator.cs
+++ Shout of Mission/Assets/LevelGenerator.cs
@@ -60,7 +60,7 @@ public class LevelGenerator : MonoBehaviour {
 
     MapModule[] westConnectors = new MapModule[] { MapModule.EW, MapModule.SW, MapModule.WN, MapModule.ESW, MapModule.SWN, MapModule.WNE, MapModule.NESW };
     MapModule[] noWestConnectors = new MapModule[] { MapModule.NS, MapModule.NE, MapModule.ES, MapModule.NES };
-    eastConnections = new MapModule[] {
+    eastConnections = new MapModule[][] {
       noWestConnectors, // empty
       noWestConnectors, // NS
       westConnectors, // EW
@@ -73,10 +73,10 @@ public class LevelGenerator : MonoBehaviour {
       noWestConnectors, // SWN
       westConnectors, // WNE
       westConnectors // NESW
-    }
+    };
 
     MapModule[] northConnectors = new MapModule[] { MapModule.NS, MapModule.NE, MapModule.WN, MapModule.NES, MapModule.SWN, MapModule.WNE, MapModule.NESW };
-    MapModule[] noNorthConnectors = new MapModule[] { MapModule.EW, MapModule.ES, MapModule.SW, MapModule.ESW }
+    MapModule[] noNorthConnectors = new MapModule[] { MapModule.EW, MapModule.ES, MapModule.SW, MapModule.ESW };
     southConnections = new MapModule[][] {
       noNorthConnectors, // empty
       northConnectors, // NS
@@ -90,11 +90,11 @@ public class LevelGenerator : MonoBehaviour {
       northConnectors, // SWN
       noNorthConnectors, // WNE
       northConnectors // NESW
-    }
+    };
 
     MapModule[] eastConnectors = new MapModule[] { MapModule.EW, MapModule.NE, MapModule.ES, MapModule.NES, MapModule.ESW, MapModule.WNE, MapModule.NESW };
-    MapModule[] noEastConnectors = new MapModule[] { MapModule.NS, MapModule.SW, MapModule.WN, MapModule.SWN }
-    westConnections = new MapModule[] {
+    MapModule[] noEastConnectors = new MapModule[] { MapModule.NS, MapModule.SW, MapModule.WN, MapModule.SWN };
+    westConnections = new MapModule[][] {
       noEastConnectors, // empty
       noEastConnectors, // NS
       eastConnectors, // EW
@@ -107,7 +107,7 @@ public class LevelGenerator : MonoBehaviour {
       eastConnectors, // SWN
       eastConnectors, // WNE
       eastConnectors // NESW
-    }
+    };
   }
 
   void InitializeMap () {
@@ -121,11 +121,11 @@ public class LevelGenerator : MonoBehaviour {
       map[r, cols - 1] = 0;
     }
 
-    Random.seed = System.DateTime.Now.Millisecond;
+    Random.InitState(System.DateTime.Now.Millisecond);
     for (int r = 1; r < rows - 1; r++) {
       for (int c = 1; c < cols - 1; c++) {
         List<MapModule> valid = GetValidMapModules (r, c);
-        map[r, c] = valid[Random.Range (0, valid.Length)];
+        map[r, c] = (int)valid[UnityEngine.Random.Range (0, valid.Count)];
       }
     }
   }
@@ -135,26 +135,78 @@ public class LevelGenerator : MonoBehaviour {
 
     for (int i = 0; i < modules.Length; i++) {
       MapModule m = modules[i];
-      if (northConnections[i].Contains(ToObject(map[row - 1, col])) && &&
-          eastConnections[i].Contains(ToObject(map[row, col + 1]))
-          southConnections[i].Contains(ToObject(map[row + 1, col])) &&
-          westConnections[i].Contains(ToObject(map[row, col - 1]))) {
+      if (ArrayContains(northConnections[i], map[row - 1, col]) &&
+          ArrayContains(eastConnections[i], map[row, col + 1]) &&
+          ArrayContains(southConnections[i], map[row + 1, col]) &&
+          ArrayContains(westConnections[i], map[row, col - 1])) {
         valid.Add(m);
       }
     }
 
-    if (valid.Length == 0) {
+    if (valid.Count == 0) {
       valid.Add(MapModule.Empty);
     }
 
     return valid;
   }
 
-  MapModule ToModule (int x) {
-    return (MapModule)Enum.ToObject(typeof(MapModule), x);
+  bool ArrayContains(MapModule[] array, int x) {
+    foreach (MapModule m in array) {
+      if (m == (MapModule)x) {
+        return true;
+      }
+    }
+    return false;
   }
 
   void DisplayMap () {
-    //
+    string output = "";
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < cols; c++) {
+        switch(map[r,c]) {
+          case 0: // empty
+            output += " ";
+            break;
+          case 1: // NS
+            output += "\u2502";
+            break;
+          case 2: // EW
+            output += "\u2500";
+            break;
+          case 3: // NE
+            output += "\u2514";
+            break;
+          case 4: // ES
+            output += "\u250c";
+            break;
+          case 5: // SW
+            output += "\u2510";
+            break;
+          case 6: // WN
+            output += "\u2518";
+            break;
+          case 7: // NES
+            output += "\u251c";
+            break;
+          case 8: // ESW
+            output += "\u252c";
+            break;
+          case 9: // SWN
+            output += "\u2524";
+            break;
+          case 10: // WNE
+            output += "\u2534";
+            break;
+          case 11: // NESW
+            output += "\u253c";
+            break;
+          default:
+            output += "X";
+            break;
+        }
+      }
+      output += "\n";
+    }
+    Debug.Log(output);
   }
 }
