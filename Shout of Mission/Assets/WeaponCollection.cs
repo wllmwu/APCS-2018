6162bace9e9f04fbfb9b4c0c0e86415acb154582
Simon <simonyu989@gmail.com>
2018-05-18 14:20:19 -0700

    Add weapon scroll and second weapon

---
+++ Shout of Mission/Assets/WeaponCollection.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+
+public class WeaponCollection : MonoBehaviour {
+
+	private int selectedWeapon = 0;
+	void Start () {
+		SelectWeapon();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int previousWeapon = selectedWeapon;
+		if (Input.GetAxis("Mouse ScrollWheel") > 0f){
+			if (selectedWeapon >= transform.childCount - 1){
+				selectedWeapon = 0;
+			}
+			else {
+				selectedWeapon ++;
+			}
+		}
+		if (Input.GetAxis("Mouse ScrollWheel") < 0f){
+			if (selectedWeapon <= 0){
+				selectedWeapon = transform.childCount - 1;
+			}
+			else {
+				selectedWeapon --;
+			}
+		}
+		if (previousWeapon != selectedWeapon){
+		SelectWeapon();
+
+		}
+	}
+
+	void SelectWeapon(){
+				int i = 0;
+		foreach(Transform weapon in transform){
+			if (i == selectedWeapon){
+				weapon.gameObject.SetActive(true);
+			}
+			else{
+				weapon.gameObject.SetActive(false);
+			}
+			i ++;
+		}
+	}
+}
