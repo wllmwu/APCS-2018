95be9272d5ef14a5ad350e57af2410f6187193a7
Simon <simonyu989@gmail.com>
2018-05-18 08:37:56 -0700

    Finish Raycasting

---
+++ Shout of Mission/Assets/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+	public float health = 100f;
+
+	public void TakeDamage(float amount){
+		health -= amount;
+		if (health <= 0){
+			Die();
+		}
+	}
+
+	void Die(){
+		Destroy(gameObject);
+	}
+}
