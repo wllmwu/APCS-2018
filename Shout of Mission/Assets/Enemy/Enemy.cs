30bfc6c32b1c42cfe0679672df79a42ef0e5b94d
William Wu <dev.wwu@gmail.com>
2018-05-23 16:38:49 -0700

    Enemy checks LOS

--- Shout of Mission/Assets/Enemy/Enemy.cs
+++ Shout of Mission/Assets/Enemy/Enemy.cs
@@ -10,8 +10,10 @@ public class Enemy : Entity {
   int pathTargetIndex;
   public bool shouldDrawGizmos;
 
+  Vector3 lookDirection;
+
   void Start() {
-    Invoke("RequestNewPath", 1);
+    Invoke("RequestNewPath", 0.1f);
   }
 
   void RequestNewPath() {
@@ -40,17 +42,52 @@ public class Enemy : Entity {
         nextWaypoint = path[pathTargetIndex];
       }
       transform.position = Vector3.MoveTowards(transform.position, nextWaypoint, speed);
-      Quaternion look = Quaternion.LookRotation(nextWaypoint - transform.position);
+      Quaternion look;
+      if (TargetInLineOfSight()) {
+        look = Quaternion.LookRotation(target.position - transform.position);
+      }
+      else {
+        look = Quaternion.LookRotation(nextWaypoint - transform.position);
+      }
       transform.rotation = Quaternion.Slerp(transform.rotation, look, Time.deltaTime * 5);
       yield return null;
     }
   }
 
+  bool TargetInLineOfSight() {
+    Debug.Log("transform: " + transform.position + " target: " + target.position);
+    lookDirection = new Vector3(target.position.x - transform.position.x, 0, target.position.z - transform.position.z);
+    Debug.Log("look direction: " + lookDirection);
+    RaycastHit hit;
+    //if (Physics.Raycast(transform.position, lookDirection, out hit)) {
+    if (Physics.Raycast(transform.position + Vector3.up, target.position - transform.position, out hit)) {
+      if (hit.transform == target) {
+        Debug.Log("in LOS");
+        return true;
+      }
+      Debug.Log("raycast hit " + hit.transform.name);
+    }
+    Debug.Log("not in LOS");
+    return false;
+  }
+
   void Fire() {
-    //
+    //muzzleFlash.Play();
+    //shootSound.Play();
+    /*RaycastHit hit;
+		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit)){
+			Debug.Log(hit.transform.name);
+		}
+
+		Entity entity = hit.transform.GetComponent<Entity>();
+		if (entity != null){
+			entity.TakeDamage(damage);
+		}*/
   }
 
   public void OnDrawGizmos() {
+    Gizmos.color = Color.blue;
+    Gizmos.DrawLine(transform.position + Vector3.up, transform.position + lookDirection + Vector3.up);
     if (shouldDrawGizmos) {
       if (path != null) {
         Gizmos.color = Color.black;
