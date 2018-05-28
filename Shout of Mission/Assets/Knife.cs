48213a3a47f6ea96203dd810478c6afbd447b553
Max Huang <maxwellhuang@Maxwells-MacBook-Pro-13.local>
2018-05-28 14:14:45 -0700

    Grenade and Knife done

---
+++ Shout of Mission/Assets/Knife.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+
+    bool slashed;
+    public Camera fpsCam;
+    public float damage;
+    public float range;
+    public GameObject impactEffect;
+    AudioSource hitSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        slashed = false;
+
+        hitSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && slashed == false)
+        {
+            transform.Rotate(0f, 0f, -70f);
+            slashed = true;
+            slash();
+        }
+        if (Input.GetButtonUp("Fire1") && slashed == true)
+        {
+            transform.Rotate(0f, 0f, 70f);
+            slashed = false;
+
+        }
+
+    }
+
+    void slash()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+        Entity entity = hit.transform.GetComponent<Entity>();
+        if (entity != null)
+        {
+            entity.TakeDamage(damage);
+            hitSound.Play();
+            GameObject temp = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(temp, 1f);
+        }
+    }
+}
